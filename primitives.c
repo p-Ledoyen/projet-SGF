fa2c4c9442ff2085d249775be3fd9cf4cde045ab
Djbrl <sydismyname123@gmail.com>
2017-06-21 05:02:11 +0200

    Add files via upload
    Commit V3

--- primitives.c
+++ primitives.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
@@ -13,9 +16,14 @@ int removeFile(char *nom);
 
 //autres primitives 
 int exec_shell(char** argv);
+int redirectionTHEN(char **argv1, char **argv2);
 
 int main(void)
 {
+	char *argv1[]={"pwd",NULL};
+	char *argv2[]={"ps",NULL};
+	char *argv[]={"ls","-l",NULL};
+	exec_shell(argv);
 	//
 	return 0;
 }
@@ -55,7 +63,7 @@ char* myread(FILE* inode, char *buffer, int nbOctets)
 		 fgets(buffer, nbOctets, inode);
 	}
 	else
-		printf("Impossible d'ecrire dans ce fichier.\n");
+		printf("Impossible de lire dans ce fichier.\n");
 	
 	return buffer;
 }
@@ -92,4 +100,71 @@ int removeFile(char *nom)
 	return 0;
 }
 
+int exec_shell(char **argv) //
+{
+	int i=0, argc;
+	char path[128] = "/bin/";
+	
+    strcat(path, argv[0]);
 
+	/*for(i=0;i<10;i++)
+	{
+		if(strcmp( argv[i], "|") == 0)
+		{
+			//erreur de segmentation
+			//printf("string #%d: %s\n", i, argv[i]);
+			//redirectionTHEN(argv1,argv2);
+		}
+		i++;
+	}*/
+
+	
+	if (execve(path, argv , NULL)==-1) //si exec retourne une erreur
+	{
+			perror("execve failed");
+			fflush(stderr);//vide le buffer
+			exit(0);
+			return -1;
+	}
+	
+	return 0;
+}
+
+int redirectionTHEN(char **argv1, char **argv2)
+{
+        int tube[2];
+        if(pipe(tube) == -1) {
+          perror("Pipe failed");
+          exit(1);
+        }
+
+        if(fork() == 0)            //first fork
+        {
+            close(STDOUT_FILENO);  //on ferme la sortie std
+            dup(tube[1]);         //on remplace stdout avec pipe write
+            close(tube[0]);       //on ferme pipe read
+            close(tube[1]);
+
+            execve(argv1[0], argv1,NULL);
+            perror("execvp failed");
+            exit(1);
+        }
+
+        if(fork() == 0)            //second fork
+        {
+            close(STDIN_FILENO);   //on ferme stdin
+            dup(tube[0]);         //on remplace stdin avec pipe read
+            close(tube[1]);       //on ferme pipe write
+            close(tube[0]);
+
+            execve(argv2[0], argv2,NULL);
+            perror("execvp 2 failed");
+            exit(1);
+        }
+        
+		close(tube[0]);
+        close(tube[1]);
+        wait();
+        wait();
+        return 0;
+}
